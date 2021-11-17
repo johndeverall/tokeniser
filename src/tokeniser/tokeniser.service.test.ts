94c38458e3c30bbe59cd8f8a18f80b73d7831a80
John Deverall <johndeverall@gmail.com>
2021-11-18 01:57:38 +1300

    Tokenizer service tests pass

--- src/tokeniser/tokeniser.service.test.ts
+++ src/tokeniser/tokeniser.service.test.ts
@@ -1,17 +1,29 @@
-import {tokenise} from "./tokeniser.service";
+import {detokenise, tokenise} from "./tokeniser.service";
 import {describe, expect, test} from '@jest/globals'
 
-describe('Tokeniser tests', () => {
-    test('Tokenise single item', () => {
-        return tokenise(['1234-1234-1234-1234']).then(data => {
-            expect(data).toBe('bla 1!')
+describe('Tokeniser tests',  () => {
+    test('Tokenise and detokenise single item', async () => {
+        const originalAccountNumber = ['1234-1234-1234-1234']
+        const tokenisedAccountNumber = await tokenise(originalAccountNumber).then(data => {
+            return data
         })
+        expect(tokenisedAccountNumber.length).toBe(1)
+        expect(tokenisedAccountNumber[0].length).toBe(32)
+        const detokenisedAccountNumber = await detokenise(tokenisedAccountNumber).then(data => data)
+        expect(detokenisedAccountNumber === originalAccountNumber)
     })
 
-    test('Tokenise multiple items', () => {
-        return tokenise(['2345-2345-2345-2345', '3456-3456-3456-3456']).then(data => {
-            expect(data).toBe('bla 2!')
+    test('Tokenise and detokenise multiple items', async () => {
+        const originalAccountNumbers = ['2345-2345-2345-2345', '3456-3456-3456-3456']
+        const tokenisedAccountNumbers = await tokenise(originalAccountNumbers).then(data => {
+            return data
         })
+        expect(tokenisedAccountNumbers.length).toBe(2)
+        expect(tokenisedAccountNumbers[0].length).toBe(32)
+        expect(tokenisedAccountNumbers[1].length).toBe(32)
+        const detokenisedAccountNumbers = await detokenise(tokenisedAccountNumbers).then(data => data)
+        expect(detokenisedAccountNumbers === originalAccountNumbers)
     })
 })
+
 
